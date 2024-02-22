972d7867c6391626cb487822d5ff292a77668c50
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:43:20 +0200

    Add project files.

---
+++ L1Ex2won7/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Scrieti un program care va calcula media aritmetica a trei numere citite de la
+tastatura*/
+
+int num1;
+Console.WriteLine("Introduce primul numar: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2;
+Console.WriteLine("Introduce al doilea număr: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int num3;
+Console.WriteLine("Introduce al treilea număr: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int media = (num1 + num2 + num3) / 3;
+
+Console.WriteLine("Media aritmetica a trei numere este :" +  media);
+
+Console.ReadLine();
